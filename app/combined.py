dd3c1e4a7b71f0a0bd8f150d3762644485205d78
ajinChen <jchen217@usfca.edu>
2022-07-20 11:56:45 -0700

    update main function

--- app/combined.py
+++ app/combined.py
@@ -1,11 +1,6 @@
 from color_select import get_color
 from color_wheel import color_output
 
-# test = {'url':['https://closettest.s3.us-west-1.amazonaws.com/trousers.jpg',
-#               'https://closettest.s3.us-west-1.amazonaws.com/beret.jpg'],
-#        'num_recommendation':2,
-#        'pattern': True}
-
 
 def cloth_rec(dictionary):
     if dictionary['pattern']:
@@ -15,6 +10,3 @@ def cloth_rec(dictionary):
     color = get_color(dictionary['urls'])
     res = color_output(color,style,dictionary['num_recommendation'])
     return res
-
-# res = cloth_rec(test)
-# print(res)
